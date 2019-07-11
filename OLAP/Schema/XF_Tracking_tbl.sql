54add873f8b3b95e3d5924076d7a856358c01b18
Chandra <Cmanchukonda@spglobal.com>
2019-07-11 07:03:23 -0400

    Updated Tacking table to add LatestEDXLogId

--- OLAP/Schema/XF_Tracking_tbl.sql
+++ OLAP/Schema/XF_Tracking_tbl.sql
@@ -1,19 +1,21 @@
 USE [OLAP]
 GO
 
-/****** Object:  Table [dbo].[XF_Tracking_tbl]    Script Date: 6/16/2019 4:51:37 AM ******/
+/****** Object:  Table [dbo].[XF_Tracking_tbl]    Script Date: 7/11/2019 7:01:59 AM ******/
 SET ANSI_NULLS ON
 GO
 
 SET QUOTED_IDENTIFIER ON
 GO
 
-IF OBJECT_ID('XF_Tracking_tbl') IS NULL
 CREATE TABLE [dbo].[XF_Tracking_tbl](
 	[TableName] [nvarchar](100) NULL,
 	[LastTrackingDate] [datetime] NULL,
-	[MaxModifyDate] [datetime] NULL
+	[MaxModifyDate] [datetime] NULL,
+	[LatestEDXLogId] [int] NULL
 ) ON [PRIMARY]
 
 GO
+
+
 
